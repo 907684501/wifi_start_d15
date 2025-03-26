905f142cff244632a33ac949fa72e5fece39df88
neeme_d15 <neeme@qq.com>
2025-03-26 11:05:51 +0800

    0.0.2

--- main/app_lib/app_lib.c
+++ main/app_lib/app_lib.c
@@ -273,9 +273,9 @@ void app_lvgl_test(void)
     lv_obj_set_style_bg_color(scr, lv_color_hex(0x000044), 0);
 
     lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_text(label, "麦上飞花 Hello World abcd 1234 中华人民共和国 ABCD!@#$^&*");
+    lv_label_set_text(label, "Hello World abcd 1234 ABCD!@#$^&*");
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_34, LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_38, LV_STATE_DEFAULT);
     lv_obj_set_style_text_color(label, lv_color_hex(0xaaaaaa), 0);
 }
 
